e5d359d3519bab3a8d7ce44bbafbc37ffeeac2dc
BALAOASalikeith <balaoaskeith@gmail.com>
2024-11-04 21:52:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let fullName = "Allysa keith Balao-as";
+let age = 19;
+let favoriteColor = "yellow";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Color:", favoriteColor);
